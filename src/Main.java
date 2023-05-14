c82ddb1ca17978bbc55839f9cc15a2c291a19098
Eco-Gaming <7ecogaming4@gmail.com>
2023-05-14 19:45:20 +0200

    Added Main Class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input = "public class Main {\n"
+                + "}";
+        System.out.println(input);
+    }
+}
