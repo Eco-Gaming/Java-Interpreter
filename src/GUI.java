ef5427f07ccfbb8fd290827a5d93f0cd133304c0
Linus <alditalk300@gmail.com>
2023-05-16 09:17:11 +0200

    Simple GUI

--- src/GUI.java
+++ src/GUI.java
@@ -6,6 +6,22 @@ public class GUI {
     public static void main(String[] args) {
         Frame frame = new Frame("Application");
 
+        Button b = new Button("Click Here");
+        b.setBounds(20,110,80,30);
+
+        // creating objects of textfield
+        TextField t1, t2;
+
+        // instantiating the textfield objects
+        // setting the location of those objects in the frame
+        t1 = new TextField("Test");
+        t1.setBounds(50, 100, 200, 30);
+        t2 = new TextField("Code hier Eingeben:");
+        t2.setBounds(50, 150, 200, 30);
+        // adding the components to frame
+        frame.add(t1);
+        frame.add(t2);
+        frame.add(b);
         frame.add(new Label("Hello!"));
         frame.setSize(500, 500);
         frame.setLocationRelativeTo(null); // Centers the window
