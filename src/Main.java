37c12f1bd2729a19b9bf8633409260731b3bf305
LRK0 <leonramin@gmx.de>
2023-05-16 17:51:27 +0200

    GUI with textfield for code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.startGUI();
         String input = "public class Main {\n"
                 + "}";
         System.out.println(input);
     }
-
 }
