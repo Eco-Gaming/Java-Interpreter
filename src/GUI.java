e1495a2ca8442ee95816603a316797d2ba8110be
LRK0 <leonramin@gmx.de>
2023-05-16 17:20:39 +0200

    better gui

--- src/GUI.java
+++ src/GUI.java
@@ -1,28 +1,28 @@
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.Label;
 
 public class GUI {
-    public static void main(String[] args) {
+    public static void startGUI() {
         Frame frame = new Frame("Application");
-
         Button b = new Button("Click Here");
-        b.setBounds(400,200,80,30);
-
+        b.setBounds(400, 200, 80, 30);
         // creating objects of textfield
-        TextField t1, t2;
+        Label l1 = new Label("Enter code here:");
 
-        // instantiating the textfield objects
-        // setting the location of those objects in the frame
-        t1 = new TextField("Test");
-        t1.setBounds(40, 100, 200, 30);
-        t2 = new TextField("Code hier Eingeben:");
-        t2.setBounds(40, 150, 200, 30);
+        l1.setBounds(100,250,120,220);
+
+        TextArea area = new TextArea("");
+        area.setBounds(120, 200, 250, 100);
+        frame.add(area);
+
         // adding the components to frame
-        frame.add(t1);
-        frame.add(t2);
         frame.add(b);
-        frame.add(new Label("Hello!"));
+        frame.add(l1);
         frame.setSize(720, 480);
         frame.setLocationRelativeTo(null); // Centers the window
 
@@ -32,7 +32,6 @@ public class GUI {
                 frame.dispose(); // Releases native screen resources
             }
         });
-
         frame.setVisible(true);
     }
 }
