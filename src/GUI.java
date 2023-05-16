8e13276380eb70a3c4aa5bb22578403bc910d80f
LRK0 <leonramin@gmx.de>
2023-05-16 09:26:10 +0200

    bigger display Button displaced

--- src/GUI.java
+++ src/GUI.java
@@ -7,7 +7,7 @@ public class GUI {
         Frame frame = new Frame("Application");
 
         Button b = new Button("Click Here");
-        b.setBounds(20,110,80,30);
+        b.setBounds(400,200,80,30);
 
         // creating objects of textfield
         TextField t1, t2;
@@ -15,15 +15,15 @@ public class GUI {
         // instantiating the textfield objects
         // setting the location of those objects in the frame
         t1 = new TextField("Test");
-        t1.setBounds(50, 100, 200, 30);
+        t1.setBounds(40, 100, 200, 30);
         t2 = new TextField("Code hier Eingeben:");
-        t2.setBounds(50, 150, 200, 30);
+        t2.setBounds(40, 150, 200, 30);
         // adding the components to frame
         frame.add(t1);
         frame.add(t2);
         frame.add(b);
         frame.add(new Label("Hello!"));
-        frame.setSize(500, 500);
+        frame.setSize(720, 480);
         frame.setLocationRelativeTo(null); // Centers the window
 
         frame.addWindowListener(new WindowAdapter() {
